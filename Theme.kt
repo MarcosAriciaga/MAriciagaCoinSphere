69633d7cadfbe3a6f302879826b9f3ad9c94cb10
MarcosAriciaga <152576071+MarcosAriciaga@users.noreply.github.com>
2025-09-13 23:27:14 -0600

    Update Theme.kt

--- Theme.kt
+++ Theme.kt
@@ -9,13 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect // Importa SideEffect
+import androidx.compose.runtime.SideEffect 
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView // Importa LocalView
-import androidx.core.view.WindowCompat // Importa WindowCompat
+import androidx.compose.ui.platform.LocalView 
+import androidx.core.view.WindowCompat 
 
-// Define los colores para el tema oscuro usando las nuevas variables
 private val DarkColorScheme = darkColorScheme(
     primary = TextMain,
     onPrimary = DarkBackground,
@@ -31,18 +30,15 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Otros colores, si se necesitan */
 )
 
 @Composable
 fun CoinSphereTheme(
-    darkTheme: Boolean = true, // Forzamos el tema oscuro a 'true'
-    dynamicColor: Boolean = false, // Desactivamos el color dinámico para usar nuestro tema
+    darkTheme: Boolean = true, 
+    dynamicColor: Boolean = false, 
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        // En este caso, el tema dinámico no se usará porque lo desactivamos
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -51,7 +47,6 @@ fun CoinSphereTheme(
         else -> LightColorScheme
     }
 
-    // Código para cambiar el color de la barra de estado
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
